4c0172a014ada4ffaa4890f1116d765500c7607a
Yaroslav Russu <97214900+YarRus12@users.noreply.github.com>
2022-05-30 22:40:42 +1000

    Прототип SQL скриптов готов

--- SQL_scripts/Final_project_DDL.sql
+++ SQL_scripts/Final_project_DDL.sql
@@ -2,108 +2,90 @@ DROP DATABASE IF EXISTS online_cinema;
 CREATE DATABASE online_cinema;
 USE online_cinema;
 
+
+DROP TABLE IF EXISTS directors;
+CREATE TABLE `directors` (
+    `id` BIGINT AUTO_INCREMENT PRIMARY KEY,
+    `name` varchar(255) NOT NULL,
+    `birth_date` DATE NOT NULL
+);
+
+DROP TABLE IF EXISTS films;
 CREATE TABLE `films` (
-    `id` INT NOT NULL,
+    `id` BIGINT AUTO_INCREMENT PRIMARY KEY,
     `film_name` varchar(255) NOT NULL,
-    `release_date` DATE(255) NOT NULL,
+    `release_date` DATE NOT NULL,
     `avr_grade` FLOAT NOT NULL,
     `description` TEXT NOT NULL,
     `duration` TIME NOT NULL,
     `film_age_rating` INT NOT NULL,
     `link` varchar(255) NOT NULL,
-    `main_director` INT NOT NULL,
-    PRIMARY KEY (`id`)
-);
+    `main_director_id` BIGINT NOT NULL,
+    FOREIGN KEY (main_director_id) REFERENCES directors (id) ON UPDATE CASCADE ON DELETE CASCADE);
 
+
+DROP TABLE IF EXISTS actors;
 CREATE TABLE `actors` (
-    `id` SERIAL NOT NULL PRIMARY KEY,
+    `id` BIGINT AUTO_INCREMENT PRIMARY KEY,
     `name` varchar(255) NOT NULL,
     `birth_date` DATE NOT NULL
-    --`films` DATE NOT NULL,
 );
 
+DROP TABLE IF EXISTS actors_in_films;
 CREATE TABLE `actors_in_films` (
-    `film_id` INT NOT NULL,
-    `actor_id` INT NOT NULL
-);
+    `film_id` BIGINT   NOT NULL,
+    `actor_id` BIGINT  NOT NULL,
+    FOREIGN KEY (film_id) REFERENCES films (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (actor_id) REFERENCES actors (id) ON UPDATE CASCADE ON DELETE CASCADE);
 
-ALTER TABLE `actors_in_films` ADD CONSTRAINT `actors_in_films_fk0`
-            FOREIGN KEY (`film_id`) REFERENCES `Films`(`id`)
-            ON UPDATE CASCADE ON DELETE CASCADE;
-ALTER TABLE `actors_in_films` ADD CONSTRAINT `actors_in_films_fk1`
-            FOREIGN KEY (`actor_id`) REFERENCES `actors`(`id`)
-            ON UPDATE CASCADE ON DELETE CASCADE;
 
-CREATE TABLE `directors` (
-    `id` INT NOT NULL,
-    `name` varchar(255) NOT NULL,
-    `birth_date` DATE NOT NULL,
-    PRIMARY KEY (`id`)
-);
-
-ALTER TABLE `Films` ADD CONSTRAINT `Films_fk0`
-            FOREIGN KEY (`main_director`) REFERENCES `directors`(`id`)
-            ON UPDATE CASCADE ON DELETE CASCADE;
-
+DROP TABLE IF EXISTS genres;  
 CREATE TABLE `genres` (
-    `id` INT NOT NULL,
-    `name` varchar(70) NOT NULL,
-    PRIMARY KEY (`id`)
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `name` varchar(70) NOT NULL
 );
 
+DROP TABLE IF EXISTS films_genres;  
 CREATE TABLE `films_genres` (
-    `film_id` INT NOT NULL,
-    `genre_id` INT(100) NOT NULL
+    `film_id` BIGINT NOT NULL,
+    `genre_id` INT NOT NULL,
+    FOREIGN KEY (`film_id`) REFERENCES `films`(`id`),
+    FOREIGN KEY (`genre_id`) REFERENCES `genres`(`id`)
 );
 
-ALTER TABLE `films_genres` ADD CONSTRAINT `films_genres_fk0`
-        FOREIGN KEY (`film_id`) REFERENCES `Films`(`id`)
-        ON UPDATE CASCADE ON DELETE CASCADE;
-ALTER TABLE `films_genres` ADD CONSTRAINT `films_genres_fk1`
-        FOREIGN KEY (`genre_id`) REFERENCES `genres`(`id`)
-        ON UPDATE CASCADE ON DELETE CASCADE;
-
+DROP TABLE IF EXISTS subscription; 
 CREATE TABLE `subscription` (
-    `id` SERIAL NOT NULL,
-    `name` VARCHAR (20)
-    `description` TEXT NOT NULL,
-    -- добавить ограниченный кргу значений и значение по дефолту
-    PRIMARY KEY (`id`)
-);
+    `id` INT PRIMARY KEY,
+    `name` VARCHAR (20),
+    `description` TEXT NOT NULL);
 
+
+DROP TABLE IF EXISTS films_id_subscription;
 CREATE TABLE `films_id_subscription` (
     `subscription_id` INT NOT NULL,
-    `film_id` INT NOT NULL
+    `film_id` BIGINT NOT NULL,
+    FOREIGN KEY (`subscription_id`) REFERENCES `subscription`(`id`) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (`film_id`) REFERENCES `films`(`id`) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-ALTER TABLE `films_id_subscription` ADD CONSTRAINT `films_id_subscription_fk0`
-            FOREIGN KEY (`subscription_id`) REFERENCES `subscription`(`id`)
-            ON UPDATE CASCADE ON DELETE CASCADE;
-ALTER TABLE `films_id_subscription` ADD CONSTRAINT `films_id_subscription_fk1`
-            FOREIGN KEY (`film_id`) REFERENCES `Films`(`id`)
-            ON UPDATE CASCADE ON DELETE CASCADE;
 
-
-CREATE TABLE `client` (
-    `id` INT NOT NULL,
+DROP TABLE IF EXISTS clients;
+CREATE TABLE `clients` (
+    `id` BIGINT AUTO_INCREMENT PRIMARY KEY,
     `name` varchar(50) NOT NULL,
     `surname` varchar(50) NOT NULL,
+    `email` varchar(50) NOT NULL,
     `birthdate` DATE NOT NULL,
-    `subscription_id` INT NOT NULL,
-    PRIMARY KEY (`id`)
-);
+    `subscription_id` INT NOT NULL DEFAULT '1',
+    `subscription_effective_to` DATE NOT NULL,
+    FOREIGN KEY (`subscription_id`) REFERENCES `subscription`(`id`) ON UPDATE CASCADE ON DELETE CASCADE);
 
-CREATE TABLE `card` (
-    `number` INT NOT NULL,
-    `client_id` INT NOT NULL,
+
+DROP TABLE IF EXISTS cards;
+CREATE TABLE `cards` (
+    `number` BIGINT NOT NULL PRIMARY KEY,
+    `owner` BIGINT NOT NULL,
     `effective_from` DATE NOT NULL,
     `effective_to` DATE NOT NULL,
-    PRIMARY KEY (`number`)
+    FOREIGN KEY (`owner`) REFERENCES `clients`(`id`) ON UPDATE CASCADE ON DELETE CASCADE
 );
-
-ALTER TABLE `client` ADD CONSTRAINT `client_fk0`
-            FOREIGN KEY (`subscription`) REFERENCES `subscription`(`id`)
-            ON UPDATE CASCADE ON DELETE CASCADE;
-ALTER TABLE `card` ADD CONSTRAINT `card_fk0`
-            FOREIGN KEY (`owner`) REFERENCES `client`(`id`)
-            ON UPDATE CASCADE ON DELETE CASCADE;
